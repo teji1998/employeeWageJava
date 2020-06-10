d30f9bc8c4bdff18da9ff5a6d66f672b8ccecd92
“teji1998” <tejasvinirpk@gmail.com>
2020-06-10 13:57:12 +0530

    Added:Total salary of employee

--- EmployeeWageUC.java
+++ EmployeeWageUC.java
@@ -1,12 +1,12 @@
-public class EmployeeWageUC{
-	public static void main(String[] args){
-		int ratePerHour=20,empHour=0,TotalHours=0;
+public class EmployeeWageUC{ public static void main(String[] args){
+		int ratePerHour=20,empHour=0,TotalHours=0,TotalDays=0;
 		System.out.println("Welcome to Employee Wage Computation.");
 		int fulltime=1;
 		int parttime=2;
 		int WorkingDays=20;
-		int i=0;
-		for (i=0;i<WorkingDays;i++){
+		int MaxHours=100;
+		while(TotalHours<MaxHours && TotalDays<WorkingDays){
+		TotalDays++;
 		double empCheck=Math.floor(Math.random()*10)%3;
 		switch((int) empCheck) {
 		case 1: empHour=8;
@@ -16,9 +16,9 @@ public class EmployeeWageUC{
 		default:empHour=0;
 				}
 	
-		System.out.println("Dailywage="+ratePerHour*empHour);
+		System.out.println("Dailywage="+(ratePerHour*empHour));
 		TotalHours+=empHour;
 		}
-		System.out.println("Monthly wage is="+(ratePerHour*TotalHours));
+		System.out.println("Total salary is="+(ratePerHour*TotalHours));
 	}
 }
