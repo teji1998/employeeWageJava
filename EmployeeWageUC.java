1f04402d1ea5c41296e166545c70415ce0ec8405
“teji1998” <tejasvinirpk@gmail.com>
2020-06-10 13:04:10 +0530

    Daily wage code

--- EmployeeWageUC.java
+++ EmployeeWageUC.java
@@ -1,13 +1,15 @@
 public class EmployeeWageUC{
 	public static void main(String[] args){
-	System.out.println("Welcome to Employee Wage Computation.");
-	int fulltime=1;
-	double empCheck=Math.floor(Math.random()*10)%2;
-	if (empCheck==fulltime){
-		System.out.println("Present");
+		int ratePerHour=20,empHour=0;
+		System.out.println("Welcome to Employee Wage Computation.");
+		int fulltime=1;
+		double empCheck=Math.floor(Math.random()*10)%2;
+		if (empCheck==fulltime){
+			empHour=8;
 		}
-	else
-		System.out.println("Absent");
+		else
+			empHour=0;
+		System.out.println("Dailywage="+ratePerHour*empHour);
 
 	}
 }
