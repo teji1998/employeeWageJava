124a91fb77e599612eeee78878cf939019aa49c7
“teji1998” <tejasvinirpk@gmail.com>
2020-06-10 12:53:50 +0530

    Checking whether employee is present or not

--- EmployeeWageUC.java
+++ EmployeeWageUC.java
@@ -1,6 +1,13 @@
-public class EmployeeWageUC {
-        public static void main (String args []) {
-                System.out.println("Welcome to Employee Wage Computation");
-        }
-}
+public class EmployeeWageUC{
+	public static void main(String[] args){
+	System.out.println("Welcome to Employee Wage Computation.");
+	int fulltime=1;
+	double empCheck=Math.floor(Math.random()*10)%2;
+	if (empCheck==fulltime){
+		System.out.println("Present");
+		}
+	else
+		System.out.println("Absent");
 
+	}
+}
